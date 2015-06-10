7f82f8775c5380960beea2aa968441902b51f555
jimjea <jimjea@gmail.com>
2015-06-10 11:04:28 -0500

    added searchmodel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,5 +14,6 @@ window.SpotSearch = {
 
 $(document).ready(function () {
     'use strict';
-    SpotSearch.init();
+    window.searchModel = new SearchModel();
+    window.searchModel.fetch();
 });
