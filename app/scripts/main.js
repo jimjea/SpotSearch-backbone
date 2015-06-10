eb5414bf8e6eefe9ea6dd70db3189cb7dac28414
jimjea <jimjea@gmail.com>
2015-06-10 10:39:43 -0500

    added initial backbone yeoman build

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global SpotSearch, $*/
+
+
+window.SpotSearch = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    SpotSearch.init();
+});
