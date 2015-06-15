1ee056bf24979123fecdce2bcb433e389e410e19
jimjea <jimjea@gmail.com>
2015-06-15 10:00:22 -0700

    added router files

---
+++ app/scripts/router/router.js
@@ -0,0 +1,14 @@
+define([
+	'jquery',
+	'backbone',
+	'views/search',
+	'views/albumsearch',
+	'views/artistsearch'
+], function($, Backbone, SearchView, AlbumSearchView, ArtistSearchView) {
+	var Router = Backbone.Router.extend({
+
+	});
+
+	return Router;
+
+});
