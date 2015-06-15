fe202f102054df0d387b11117090f6b366a421ef
jimjea <jimjea@gmail.com>
2015-06-15 16:16:53 -0700

    created album view skeleton'

---
+++ app/scripts/views/album.js
@@ -0,0 +1,9 @@
+define([
+	'backbone',
+	'jquery',
+	'handlebars',
+	'text!templates/loading.hbs',
+	'text!templates/album.hbs'
+], function(Backbone, $, Handlebars, loadingTemplate, albumTemplate) {
+
+});
