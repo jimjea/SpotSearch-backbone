f190b48ce29686f8db19a1c034d32f4e5482ef44
jimjea <jimjea@gmail.com>
2015-06-15 16:28:24 -0700

    completed album view functionality

--- app/scripts/views/album.js
+++ app/scripts/views/album.js
@@ -5,8 +5,35 @@ define([
 	'text!templates/loading.hbs',
 	'text!templates/album.hbs'
 ], function(Backbone, $, Handlebars, loadingTemplate, albumTemplate) {
+
 	var AlbumView = Backbone.View.extend({
-		
+
+		events: {
+			'click .track': 'playTrack'
+		},
+
+		initialize: function() {
+			this.listenTo(this.model, 'request', this.loading);
+			this.listenTo(this.model, 'change', this.render);
+		},
+
+		render: function() {
+			var date = this.model.toJSON();
+			var template = Handlebars.compile(albumTemplate);
+			this.$el.html(template(data));
+		},
+
+		loading: function() {
+			var template = Handlebars.compile(loadingTemplate);
+			this.$el.html(template({}));
+		},
+
+		playTrack: function(e) {
+			e.preventDefault();
+			$('#player').attr('src': e.currentTarget.attributes.href.value);
+			$('#player').trigger('play');
+		}
+
 	});
 
 	return AlbumView;
