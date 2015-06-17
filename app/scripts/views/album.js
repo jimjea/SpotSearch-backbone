1fb009da1ae6cb8d88ea2583a581f344894b2844
jimjea <jimjea@gmail.com>
2015-06-17 13:31:45 -0700

    updated views to reflect playlist functionality

--- app/scripts/views/album.js
+++ app/scripts/views/album.js
@@ -2,14 +2,16 @@ define([
 	'backbone',
 	'jquery',
 	'handlebars',
+	'models/playlist',
 	'text!templates/loading.hbs',
 	'text!templates/album.hbs'
-], function(Backbone, $, Handlebars, loadingTemplate, albumTemplate) {
+], function(Backbone, $, Handlebars, PlaylistModel, loadingTemplate, albumTemplate) {
 
 	var AlbumView = Backbone.View.extend({
 
 		events: {
-			'click .track': 'playTrack'
+			'click .track': 'playTrack',
+			'click #add-playlist': 'addPlaylist'
 		},
 
 		initialize: function() {
@@ -32,6 +34,21 @@ define([
 			e.preventDefault();
 			$('#player').attr({'src': e.currentTarget.attributes.href.value});
 			$('#player').trigger('play');
+		},
+
+		addPlaylist: function(e) {
+			e.preventDefault();
+			var dateString = new Date().toISOString();
+			var newPlaylistName = "Playlist "+ dateString;
+			var newPlaylistTrack = {
+			  name: 'dummy track name',
+			  artist: 'Some Artist',
+			  album: 'Some Album'
+			};
+			var newPlaylist = new PlaylistModel({name: newPlaylistName});
+			newPlaylist.set('tracks', [newPlaylistTrack]);
+			this.collection.add(newPlaylist);
+			newPlaylist.save();
 		}
 
 	});
